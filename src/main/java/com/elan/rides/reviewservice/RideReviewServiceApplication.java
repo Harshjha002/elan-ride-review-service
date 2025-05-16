dfbba4cb6f1614c8e1accffe5d5fa7beaf8e90b6
Harsh <harshjha8453@gmail.com>
2025-05-16 13:02:16 +0530

    chore: add basic application configuration

---
+++ src/main/java/com/elan/rides/reviewservice/RideReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elan.rides.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elan/rides/reviewservice/RideReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elan.rides.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
